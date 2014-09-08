b1bdd8e34b13c5d59315cbe3eb338b4e04ee6b26
LoVka <lovkavo@gmail.com>
2014-09-08 23:20:08 +0300

    View of the client

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   validates :description, :image_url, :title, presence: true
   validates :price, numericality: {greater_that_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: {minimum: 10}
   validates :image_url, allow_blank: true, format: {
 	with: %r{\.(gif|jpg|png)$}i,
 	message: 'URL must be GIF, JPG or PNG.'
