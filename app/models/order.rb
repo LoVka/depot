b13dcd366c47ae267378869d7d73fa741fe59fc6
LoVka <lovkavo@gmail.com>
2014-09-18 16:23:57 +0300

    Order

---
+++ app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ActiveRecord::Base
+  attr_accessible :address, :email, :name, :pay_type
+
+    PAYMENT_TYPES = ["Check", "Credit cart", "Purchase order" ]
+
+    validates :name, :address, :email, presence: true
+    validates :pay_type, inclusion: PAYMENT_TYPES
+end
